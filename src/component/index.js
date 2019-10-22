b5003419ad322366c45064c48d86ad668ec0f91b
alex-kostenko <0984181760ss@gmail.com>
2019-10-22 13:45:34 +0300

    add jss

--- src/component/index.js
+++ src/component/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
 
-const MainPage = () => {
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: theme.spacing(6, 0, 3),
+  },
+  lightBulb: {
+    verticalAlign: 'middle',
+    marginRight: theme.spacing(1),
+  },
+}));
 
+const MainPage = (props) => {
+
+  const classes = useStyles();
+  console.log(classes);
+  
   return (
-    <ul>
+    <ul className={classes.root}>
       <li><Link to="/">Home</Link></li>
       <li><Link to="/registration">registration</Link></li>
       <li><Link to="/user">user</Link></li>
