e6d63306b1aa2f798d8180713fdf4091052518c1
alex-kostenko <0984181760ss@gmail.com>
2019-10-22 12:39:35 +0300

    Authentication for google mail

---
+++ src/component/registration.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+const RenderRegistraion = () => {
+  
+  return (
+    <form action="" method="POST" id="registrationform">
+      <h1>Register</h1>
+      <div class="alert">Message Sent</div>
+      <fieldset>
+
+      <legend><span class="section">1</span>Your Basic Info</legend>
+        <label class="" for="name">Name:</label>
+        <input type="text" name="name" value="" id="name" required />
+
+        <label class="" for="email">Email:</label>
+        <input type="email" name="email" value="" id="email" required />
+
+        <label for="password">password:</label>
+        <input type="password" name="password" value="" id="password" required />
+      </fieldset>
+
+      <fieldset>
+
+      <legend><span class="section">2</span>Profile</legend>
+
+        <label class="" for="bio">Bio:</label>
+        <textarea name="bio" id="bio" required></textarea>
+
+        <label for="job">Job Role:</label>
+        <select name="job" id="job" required>
+          <optgroup label="Web">
+            <option value="front_end_developer" id="frontend">Frontend Developer</option>
+            <option value="back_end_developer" id="backend">Backend Developer</option>
+            <option value="fullstack_developer" id="fullstack">Fullstack Developer</option>
+          </optgroup>
+          <optgroup label="Mobile">
+            <option value="android" id="android">Android</option>
+            <option value="ionic" id="ionic">Ionic</option>
+            <option value="phonegap" id="phonegap">PhoneGap</option>
+          </optgroup>
+        </select>
+        <br /><br />
+        <label>Interest:</label>
+
+        <select id="interest" required>
+          <option value="development">Development</option>
+          <option value="design">Design</option>
+          <option value="business">Business</option>
+        </select>
+
+
+        <button type="submit" >Register</button>
+        </fieldset>
+    </form>
+  )
+}
---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyBH_5JwsUyke4mfst5zf1BS-CjaquLj-Cw",
+  authDomain: "pet-prj-backend.firebaseapp.com",
+  databaseURL: "https://pet-prj-backend.firebaseio.com",
+  projectId: "pet-prj-backend",
+  storageBucket: "pet-prj-backend.appspot.com",
+  messagingSenderId: "166070696432",
+  appId: "1:166070696432:web:c1ef7f6450d29bae798535",
+  measurementId: "G-W7JNTPKN46"
+};
